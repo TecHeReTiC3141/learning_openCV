1089f097c2562363fbb4692ff6b80fd8e1ee8244
TecHeReTiC <talex73@mail.ru>
2022-04-16 21:17:21 +0500

    Some scratches

--- scripts/reading_images.py
+++ scripts/reading_images.py
@@ -1,3 +1,22 @@
 import cv2 as cv
+sea = cv.imread('../resources/images/sea.jpg')
 
-print(cv.version.opencv_version)
+cv.imshow('sea', sea)
+
+cv.waitKey(0)
+
+capture = cv.VideoCapture('../resources/videos/Fireplace - 1971.mp4')
+
+while True:
+    try:
+        _, frame = capture.read()
+        cv.imshow('Campfire', frame)
+    except cv.error as e:
+        print(e)
+        break
+
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
